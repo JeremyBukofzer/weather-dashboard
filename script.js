895c2dbc1d979538587838446863e762fbcedd5b
Jeremy Bukofzer <jlbukofzer@gmail.com>
2022-10-03 19:06:47 -0500

    adds search and pseudo code

---
+++ script.js
@@ -0,0 +1,10 @@
+// select elements from DOM
+    // form
+    // input
+
+// listen for submit event
+    //get city out of input
+    // fetch city's weather
+        // show weather in card
+            //fetch five-day forecast for city
+                // show forecast in cards

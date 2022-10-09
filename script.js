0736da6667c3818475f299f41f6709fc077b37d4
Jeremy Bukofzer <jlbukofzer@gmail.com>
2022-10-09 16:14:14 -0500

    tests API

--- script.js
+++ script.js
@@ -2,9 +2,14 @@
     // form
     // input
 
+
 // listen for submit event
     //get city out of input
     // fetch city's weather
         // show weather in card
             //fetch five-day forecast for city
                 // show forecast in cards
+
+fetch("https://api.openweathermap.org/data/2.5/weather?q=milwaukee&appid=47a293b2c262948368bb05e085504ccd")
+    .then(res => res.json())
+    .then(data => console.log(data))

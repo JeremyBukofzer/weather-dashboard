26a264eefba775fefc9fcebc31d26ffcd91e1b40
Jeremy Bukofzer <jlbukofzer@gmail.com>
2022-10-12 15:57:05 -0500

    adds function to render forecast data

--- script.js
+++ script.js
@@ -19,9 +19,7 @@ var getWeather = function (cityName) {
                 var todaysDate = response.dt;
                 var date = moment.unix(todaysDate).format("MM/DD/YY")
                 $("#date-today").html(date);
-                var weatherIcon =  "http://openweathermap.org/img/wn/" +
-                response.weather[0].icon +
-                "@2x.png";
+                var weatherIcon =  "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png";
                 $(current-icon).attr("src", weatherIcon)
 
                 var lat = response.coord.lat;
@@ -46,6 +44,32 @@ var getUvIndex = function (lat, lon) {
         $("#uv-index").html(response.value)
     })
 };
+
+var getForecastData = function (lat,lon) {
+
+    var forecastApi = weatherApi + "&lat=" + lat + "&lon=" + lon + apiKey + units;
+    fetch(forecastApi)
+    .then(function (response) {
+        return response.json();
+    })
+    .then (function  () {
+        for (var i = 1; i < 6; i++) {
+
+            var futuredates = response.daily[i].dt;
+            var date = moment.unix(futuredates).format("dddd, MM Do")
+            $('#forecast-date' + i).html(date);
+
+            var forecastIcon = "http://openweathermap.org/img/wn/" + response.daily[i].weather[0].icon + "@2x.png";
+            $("#forecast-img" + i).html(forecastIcon);
+            var forecastTemp = response.daily[i].temp.day;
+            $("#temp-day" + i).html(forecastTemp);
+            var forecastWind = response.daily[i].wind.speed;
+            var forecastHumidity = response.daily[i].humidity;
+            $("#huimidity-day" + i).html(forecastHumidity);
+        }
+    })
+};
+
 
 
         

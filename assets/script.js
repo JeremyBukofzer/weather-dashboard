4496cd48a11b864a0b28ca2955727e1b5bbaf03b
Jeremy Bukofzer <jlbukofzer@gmail.com>
2023-01-02 20:35:07 -0600

    adds weather display function

--- assets/script.js
+++ assets/script.js
@@ -25,5 +25,23 @@ let getCityWeather = function(city) {
 let searchSubmitHandler = function(event) {
     event.preventDefault();
 
-    let cityName = $("#search-city")
+    let cityName = $("#search-city").val().trim();
+
+    if(cityName) {
+        getCityWeather(cityName);
+
+        $("#search-city").val("");
+    } else {
+        alert("Please enter the name of a city");
+    }
+};
+
+let displayWeather = function(weatherData) {
+
+    $("#city-name").text(weatherData.name + " (" + dayjs(weatherData.dt * 1000).format("MM/DD/YYYY") + ") ").append(`<img src="https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png"></img>`);
+    $("#temp").text("Temperature: " + weatherData.main.temp.toFixed(1) + "°F");
+    $("#wind").text("Wind Speed: " + weatherData.wind.speed.toFixed(1) + "mph");
+    $("#humidity").text("Humidity: " + weatherData.main.humidity + "%");
+
+    fetch
 }
